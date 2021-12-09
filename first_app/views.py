b75fd5ea1fe6cbc957ba3e49891684e11a121d55
Geetha <k.geetha@innovaccer.com>
2021-12-09 19:42:11 +0530

    add registration functionality and form page

--- first_app/views.py
+++ first_app/views.py
@@ -10,3 +10,19 @@ from datetime import date
 
 def index(request):
     return render(request, 'first_app/index.html')
+
+def register(request):
+    registered  = False
+    if request.method == "POST":
+        user_form=Userform(data=request.POST)
+        if user_form.is_valid():
+            user=user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:
+        user_form = Userform()
+    return render(request,'first_app/registration.html',{'user_form':user_form,
+                                                         'registered':registered})
